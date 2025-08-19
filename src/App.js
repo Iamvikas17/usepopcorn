712a03a506fd8d6680572a70b375f670f43a930a
Vikas Yadav <vikas.vikasyadav2005@gmail.com>
2025-08-19 11:58:14 +0530

    Fixed the Bug(https)

--- src/App.js
+++ src/App.js
@@ -281,7 +281,7 @@ function MovieDetail({ selectedId, closeButton, onAddWatched, watched }) {
       setIsLoading(true);
       async function getMovieDetail() {
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         console.log(data);
